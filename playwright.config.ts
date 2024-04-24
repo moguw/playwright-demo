73e38ba9cacf43da65db0c853e164fc6a008be69
Nan <nan@Edison-Wang.local>
2024-04-24 11:35:18 +0800

    slack message notify

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['playwright-json-summary-reporter'],
+    ['html'], // other reporters
+    ['dot']
+  ],
   outputDir: 'test-results',
   globalSetup: './utils/ENV-setup.ts',
   expect: {
