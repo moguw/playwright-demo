70d4b3ee42833f37679f2da7a4c30d823a038de1
EdisonW <nan@Edison-Wang.local>
2024-04-26 18:15:06 +0800

    update role case

--- testCase/Brands/brandSetting/user.spec.ts
+++ testCase/Brands/brandSetting/user.spec.ts
@@ -1,13 +1,21 @@
 import { test,expect } from '../../../utils/Fixture-setup';
 import { User } from '../../../Page/Brands/brandSetting/user.page'
 
-
-test.describe('test - delete user',() => {
-    test('orgOwner', {tag: '@user'},
-        async ({ orgOwner }) => {
-            // page is authenticated as admin
-            const orgOwnerRole = new User(orgOwner.page)
-            await orgOwnerRole.goToBrandSettingPage()
-            // await orgOwnerRole.deleteUser('Brand Owner')
-    }); 
-  });
+test.describe('test brand-user case',()=>{
+    /**
+     * test.beforeEach 执行测试用例前的准备工作
+     */
+    let orgOwnerRole: User
+  
+    test.beforeEach(async ({ orgOwner }) => {
+        orgOwnerRole = new User(orgOwner.page)
+    })
+    test.describe('test - delete user',() => {
+        test('orgOwner', {tag: '@brand-user'},
+            async ({ }) => {
+                // page is authenticated as orgOwner
+                await orgOwnerRole.goToBrandSettingPage()
+                await orgOwnerRole.deleteUser('Brand Owner')
+        }); 
+    });
+})
--- testCase/Brands/groups.spec.ts
+++ testCase/Brands/groups.spec.ts
@@ -3,32 +3,37 @@ import { Groups } from '../../Page/Brands/group.page'
 /**create Group
  * crate Group after admin user login 
  */
+test.describe('test group case',()=>{
+  /**
+   * test.beforeEach 执行测试用例前的准备工作
+   */
+  let orgOwnerRole, groupOwnerRole: Groups
 
-test.describe('test - create group',() => {
-  test('orgOwner', {tag: '@group'},
-      async ({ orgOwner }) => {
-          // page is authenticated as admin
-          const orgOwnerRole = new Groups(orgOwner.page)
-          await orgOwnerRole.goToOverviewPage()
-          await orgOwnerRole.createGroup()
-          await orgOwnerRole.checkGroupElementIsExsit(1)
+  test.beforeEach(async ({ orgOwner,groupOwner }) => {
+      orgOwnerRole = new Groups(orgOwner.page)
+      groupOwnerRole = new Groups(groupOwner.page)
+  })
+  test.describe('test - create group',() => {
+    test('orgOwner', {tag: '@group'},
+        async ({ }) => {
+            // page is authenticated as orgOwner
+            await orgOwnerRole.goToOverviewPage()
+            await orgOwnerRole.createGroup()
+            await orgOwnerRole.checkGroupElementIsExsit(1)
+    });
+    test('groupOwner', {tag: '@group'},
+        async ({ }) => {
+            // page is authenticated as groupOwner
+            await groupOwnerRole.goToOverviewPage()
+            await groupOwnerRole.checkGroupElementIsExsit(0)
+    });  
   });
-  test('groupOwner', {tag: '@group'},
-      async ({ groupOwner }) => {
-          // page is authenticated as admin
-          const groupOwnerRole = new Groups(groupOwner.page)
-          await groupOwnerRole.goToOverviewPage()
-          await groupOwnerRole.checkGroupElementIsExsit(0)
-  });  
-});
-
-
-test.describe('test - delete group',() => {
-  test('orgOwner', {tag: '@group'},
-      async ({ orgOwner }) => {
-          // page is authenticated as admin
-          const orgOwnerRole = new Groups(orgOwner.page)
-          await orgOwnerRole.goToOverviewPage()
-          await orgOwnerRole.deleteGroup()
-  });  
-});
+  test.describe('test - delete group',() => {
+    test('orgOwner', {tag: '@group'},
+        async ({ }) => {
+            // page is authenticated as orgOwner
+            await orgOwnerRole.goToOverviewPage()
+            await orgOwnerRole.deleteGroup()
+    });  
+  });
+})
